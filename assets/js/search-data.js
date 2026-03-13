e5a13ffff86d1688e9dbdb292021c47f6b76d670
niharikascode <107383684+niharikascode@users.noreply.github.com>
2026-03-13 05:23:59 +0000

    Deploying to gh-pages from @ niharikascode/niharikascode.github.io@7cf586308f9413b6f72dbc4b9b4b075139a8a80b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,10 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{
+        },{id: "news-karpathy-s-autoresearch-tool-lets-ai-optimize-code-overnight-autoresearch-is-an-open-source-tool-with-three-simple-files-a-data-prep-script-a-training-loop-to-modify-and-program-md-for-agent-instructions-it-runs-quick-experiments-commits-improvements-via-git-and-plans-smarter-next-steps-turning-nanochat-s-gpt-2-benchmark-from-2-02-hours-to-1-80-hours-through-tweaks-like-better-attention-scalers-replications-spread-fast-yuchen-jin-ran-568-experiments-in-11-hours-on-8-gpus-jaber-abuzaid-boosted-gpu-kernels-10x-over-baseline-and-others-applied-it-to-trading-strategies-gaining-22-returns-karpathy-launched-agenthub-as-a-git-based-hub-for-agent-swarms-hinting-at-agent-driven-research-replacing-solo-human-work",
+          title: 'Karpathy’s Autoresearch Tool Lets AI Optimize Code Overnight. Autoresearch is an open-source tool...',
+          description: "",
+          section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
